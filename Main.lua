3138825f6fcd8b42dea42d252e1deb05c346d233
Ron Jeffries <ronjeffries@acm.org>
2020-05-11 11:15:02 -0400

    Moving Circle Wraps Around

--- Main.lua
+++ Main.lua
@@ -2,10 +2,23 @@
 -- RJ 20200511
 
 function setup()
-    print("Hello World!")
+    print("Hello Asteroids!")
+    X = 400
+    Y = 500
 end
 
 function draw()
     background(40, 40, 50)
+    strokeWidth(1)
+    stroke(255)
+    fill(40, 40,50)
+    ellipseMode(CENTER)
+    ellipse(X,Y, 120)
+    X = X + 1
+    if X > WIDTH then X = X - WIDTH end
+    if X < 0 then X = X + WIDTH end
+    Y = Y + 2
+    if Y > HEIGHT then Y = Y - HEIGHT end
+    if Y < 0 then Y = Y + HEIGHT end
 end
 
