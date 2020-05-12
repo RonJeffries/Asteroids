40d8506c19fa98eda8efc2134be0e4e22d619e08
Ron Jeffries <ronjeffries@acm.org>
2020-05-12 10:58:25 -0400

    rectangle starts moving at random angle

--- Main.lua
+++ Main.lua
@@ -3,22 +3,23 @@
 
 function setup()
     print("Hello Asteroids!")
-    X = 400
-    Y = 500
+    Pos = vec2(400,500)
+    Angle = math.random()*2*math.pi
+    Vel = 1.5
 end
 
 function draw()
     background(40, 40, 50)
-    strokeWidth(1)
     stroke(255)
     fill(40, 40,50)
-    ellipseMode(CENTER)
-    ellipse(X,Y, 120)
-    X = X + 1
-    if X > WIDTH then X = X - WIDTH end
-    if X < 0 then X = X + WIDTH end
-    Y = Y + 2
-    if Y > HEIGHT then Y = Y - HEIGHT end
-    if Y < 0 then Y = Y + HEIGHT end
+    strokeWidth(2)
+    rectMode(CENTER)
+    rect(Pos.x, Pos.y, 120)
+    local step = vec2(Vel,0):rotate(Angle)
+    Pos = Pos + step
+    if Pos.x > WIDTH then Pos.x = Pos.x - WIDTH end
+    if Pos.x < 0 then Pos.x = Pos.x + WIDTH end
+    if Pos.y > HEIGHT then Pos.y = Pos.y - HEIGHT end
+    if Pos.y < 0 then Pos.y = Pos.y + HEIGHT end
 end
 
--- TestAsteroids.lua
+++ TestAsteroids.lua
@@ -17,6 +17,37 @@ function testAsteroids()
         _:test("Hookup", function()
             _:expect( 2+1 ).is(3)
         end)
+        
+        _:test("Random", function()
+            local min = 100
+            local max = 0
+            for i = 0,1000 do
+                local rand = math.random()*2*math.pi
+                if rand < min then min = rand end
+                if rand > max then max = rand end
+            end
+            _:expect(min < 0.01).is(true)
+            _:expect(max > 6.2).is(true)
+        end)
+        
+        _:test("Rotated Length", function()
+            for i = 0, 1000 do
+                local rand = math.random()*2*math.pi
+                local v = vec2(1.5,0):rotate(rand)
+                local d = v:len()
+                _:expect(d > 1.495).is(true)
+                _:expect(d < 1.505).is(true)
+            end
+        end)
+        
+        _:test("Some rotates go down", function()
+            local angle = math.rad(-45)
+            local v = vec2(1,0):rotate(angle)
+            local rvx = v.x*1000//1
+            local rvy = v.y*1000//1
+            _:expect(rvx).is(707)
+            _:expect(rvy).is(-708)
+        end)
 
     end)
 end
