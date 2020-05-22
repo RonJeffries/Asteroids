a098861c8cf476ebd1448c32700c72d16bf3859b
Ron Jeffries <ronjeffries@acm.org>
2020-05-22 12:10:15 -0400

    Spray missiles

--- Missile.lua
+++ Missile.lua
@@ -8,12 +8,12 @@ function drawMissiles()
     pushMatrix()
     fill(255)
     stroke(255)
-    for i,missile in ipairs(Missiles) do
+    for k, missile in pairs(Missiles) do
         ellipse(missile.pos.x, missile.pos.y, 6)
     end
     popMatrix()
     popStyle()
-    for i, missile in ipairs(Missiles) do
+    for k, missile in pairs(Missiles) do
         missile.pos = missile.pos + missile.vel
         missile.pos = vec2(keepInBounds(missile.pos.x, WIDTH), keepInBounds(missile.pos.y, HEIGHT))
     end
@@ -24,9 +24,13 @@ Missile = class()
 local MissileVelocity = 2.0
 
 function Missile:init(ship)
+    function die()
+        Missiles[self] = nil
+    end
     self.pos = ship.pos
     self.vel = vec2(MissileVelocity,0):rotate(math.rad(ship.ang))
-    table.insert(Missiles, self)
+    Missiles[self] = self
+    tween(3, self, {}, tween.easing.linear, die)
 end
 
 function Missile:draw()
